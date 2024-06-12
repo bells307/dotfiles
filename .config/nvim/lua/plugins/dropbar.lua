81c350c8061f3556fb7d4c0f694dcc45594c8ae3
Bells307 <sdikansky96@gmail.com>
2024-06-12 11:23:24 +0300

    upd

--- .config/nvim/lua/plugins/dropbar.lua
+++ .config/nvim/lua/plugins/dropbar.lua
@@ -1,12 +1,12 @@
 return {
   {
     "Bekaboo/dropbar.nvim",
-    -- optional, but required for fuzzy finder support
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
     },
-    config = function()
-      vim.keymap.set("n", "<leader>D", ":lua require('dropbar.api').pick()<CR>", { desc = "Dropbar pick" })
-    end,
+    event = { "BufReadPre" },
+    keys = {
+      { "<leader>D", ":lua require('dropbar.api').pick()<CR>", desc = "Dropbar pick" },
+    },
   },
 }
--- .wezterm.lua
+++ .wezterm.lua
@@ -11,6 +11,6 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 8
+config.macos_window_background_blur = 20
 
 return config
