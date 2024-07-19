dfef7843e31d17ea21710890a8abcaf1d23fe6a5
Bells307 <sdikansky96@gmail.com>
2024-07-19 15:26:03 +0300

    upd

--- .config/nvim/lua/plugins/term.lua
+++ .config/nvim/lua/plugins/term.lua
@@ -11,8 +11,8 @@ return {
     config = function(opts)
       require("FTerm").setup(opts)
 
-      vim.keymap.set("n", "<C-t>", '<CMD>lua require("FTerm").toggle()<CR>')
-      vim.keymap.set("t", "<C-t>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+      vim.keymap.set("n", "<C-\\>", '<CMD>lua require("FTerm").toggle()<CR>')
+      vim.keymap.set("t", "<C-\\>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
     end,
   },
 }
