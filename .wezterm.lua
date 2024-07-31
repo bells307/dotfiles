0fcb1d0846c1189ead80b22bc3d99cf70153d4e7
Bells307 <sdikansky96@gmail.com>
2024-07-31 23:19:16 +0300

    upd

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,13 +4,11 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Macchiato"
 
-config.font = wezterm.font("Fira Code")
-config.font_size = 14
+config.font = wezterm.font("Fira Code Nerd Font")
+config.font_size = 15
 
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 20
 
 return config
