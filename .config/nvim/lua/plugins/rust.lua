b324cb442d9b33891c4ac5ce5cc0fc85fbee58f5
Bells307 <sdikansky96@gmail.com>
2024-09-21 20:28:25 +0300

    upd

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -40,9 +40,6 @@ return {
     opts = {
       server = {
         on_attach = function(_, bufnr)
-          -- vim.keymap.set("n", "<leader>ca", function()
-          --   vim.cmd.RustLsp("codeAction")
-          -- end, { desc = "Code Action", buffer = bufnr })
           vim.keymap.set("n", "<leader>dr", function()
             vim.cmd.RustLsp("debuggables")
           end, { desc = "Rust Debuggables", buffer = bufnr })
@@ -77,12 +74,6 @@ return {
     },
     config = function(_, opts)
       vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
-      if vim.fn.executable("rust-analyzer") == 0 then
-        LazyVim.error(
-          "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
-          { title = "rustaceanvim" }
-        )
-      end
     end,
   },
 
