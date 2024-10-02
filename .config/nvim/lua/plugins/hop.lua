cf2bc76dc2e85047116d36dc6ca3b127cb8668fb
Bells307 <sdikansky96@gmail.com>
2024-10-02 12:34:56 +0300

    upd

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -3,7 +3,7 @@ return {
     "phaazon/hop.nvim",
     event = "BufReadPre",
     keys = {
-      { "\\", "<cmd>HopWord<cr>", desc = "Hop Word" },
+      { "s", "<cmd>HopWord<cr>", desc = "Hop Word" },
       { "<leader>hw", "<cmd>HopWord<cr>", desc = "Hop Word" },
       { "<leader>ha", "<cmd>HopAnywhere<cr>", desc = "Hop Anywhere" },
     },
