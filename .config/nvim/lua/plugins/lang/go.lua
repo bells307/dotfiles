28c280cec8f6f65f80ff5b8b5f6b7e066e382982
Bells307 <sdikansky96@gmail.com>
2024-10-18 00:07:03 +0300

    upd

--- .config/nvim/lua/plugins/lang/go.lua
+++ .config/nvim/lua/plugins/lang/go.lua
@@ -1,7 +1,6 @@
 return {
   {
     "mfussenegger/nvim-dap",
-    optional = true,
     dependencies = {
       {
         "williamboman/mason.nvim",
@@ -10,6 +9,51 @@ return {
       {
         "leoluz/nvim-dap-go",
         opts = {},
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
       },
     },
   },
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,149 +1,74 @@
+local function lsp_config(_, opts)
+  vim.diagnostic.config({
+    virtual_text = true,
+    underline = true,
+    float = { border = "rounded" },
+  })
+
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+    callback = function(event)
+      local map = function(keys, func, desc)
+        vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc })
+      end
+
+      map("gd", require("telescope.builtin").lsp_definitions, "Go to definition")
+      map("gD", vim.lsp.buf.declaration, "Go to declaration")
+      map("gr", require("telescope.builtin").lsp_references, "Go to references")
+      map("gI", require("telescope.builtin").lsp_implementations, "Go to implentations")
+      map("gy", require("telescope.builtin").lsp_type_definitions, "Type definition")
+      map("<leader>cs", require("telescope.builtin").lsp_document_symbols, "Document symbols")
+      map("<leader>cS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace symbols")
+      map("<leader>cr", vim.lsp.buf.rename, "Rename")
+      map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+      map("<leader>cl", vim.lsp.codelens.refresh, "CodeLens Refresh")
+      map("<leader>cR", vim.lsp.codelens.run, "CodeLens Run")
+      map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+      local client = vim.lsp.get_client_by_id(event.data.client_id)
+      if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+        vim.lsp.inlay_hint.enable(true)
+        map("<leader>th", function()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
+        end, "Toggle Inlay Hints")
+      end
+    end,
+  })
+
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+  require("mason").setup()
+
+  local ensure_installed = vim.tbl_keys(opts.servers or {})
+  vim.list_extend(ensure_installed, {
+    "stylua",
+    "gopls",
+    "pyright",
+  })
+
+  require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+  require("mason-lspconfig").setup({
+    handlers = {
+      function(server_name)
+        local server = opts.servers[server_name] or {}
+        server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+        require("lspconfig")[server_name].setup(server)
+      end,
+    },
+  })
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+      { "williamboman/mason.nvim", config = true },
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
       { "folke/neodev.nvim", opts = {} },
     },
-    config = function()
-      vim.diagnostic.config({
-        virtual_text = true,
-        underline = true,
-        float = { border = "rounded" },
-      })
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc })
-          end
-
-          map("gd", require("telescope.builtin").lsp_definitions, "Go to definition")
-          map("gD", vim.lsp.buf.declaration, "Go to declaration")
-          map("gr", require("telescope.builtin").lsp_references, "Go to references")
-          map("gI", require("telescope.builtin").lsp_implementations, "Go to implentations")
-          map("gy", require("telescope.builtin").lsp_type_definitions, "Type definition")
-          map("<leader>cs", require("telescope.builtin").lsp_document_symbols, "Document symbols")
-          map("<leader>cS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace symbols")
-          map("<leader>cr", vim.lsp.buf.rename, "Rename")
-          map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-          map("<leader>cl", vim.lsp.codelens.refresh, "CodeLens Refresh")
-          map("<leader>cR", vim.lsp.codelens.run, "CodeLens Run")
-          map("K", vim.lsp.buf.hover, "Hover Documentation")
-
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-            vim.lsp.inlay_hint.enable(true)
-            map("<leader>th", function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
-            end, "Toggle Inlay Hints")
-          end
-        end,
-      })
-
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = "Replace",
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
-        gopls = {
-          settings = {
-            gopls = {
-              gofumpt = true,
-              codelenses = {
-                gc_details = false,
-                generate = true,
-                regenerate_cgo = true,
-                run_govulncheck = true,
-                test = true,
-                tidy = true,
-                upgrade_dependency = true,
-                vendor = true,
-              },
-              hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-              },
-              analyses = {
-                fieldalignment = true,
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-              },
-              usePlaceholders = true,
-              completeUnimported = true,
-              staticcheck = true,
-              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-              semanticTokens = true,
-            },
-          },
-        },
-      }
-
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
-      require("mason").setup()
-
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        "stylua", -- Used to format Lua code
-        "gopls",
-      })
-      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-      require("mason-lspconfig").setup({
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
-            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-            require("lspconfig")[server_name].setup(server)
-          end,
-        },
-      })
-    end,
+    config = lsp_config,
   },
 }
