f1f9bfca229d7f985b119d57ce4427ca984b3336
bells307 <bells.dev@proton.me>
2026-02-24 11:13:46 +0300

    themes

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,4 +5,8 @@ require("core.russian")
 require("core.lsp")
 require("core.statusline")
 
-vim.cmd.colorscheme("kanagawa")
+local theme_file = io.open(vim.fn.expand("~/.config/current-theme"), "r")
+local current_theme = theme_file and theme_file:read("*l") or "dark"
+if theme_file then theme_file:close() end
+
+vim.cmd.colorscheme(current_theme == "light" and "github_light" or "kanagawa-wave")
