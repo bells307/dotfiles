560b66a6f05194631a00593516a0721ca8721160
bells307 <bells.dev@proton.me>
2026-04-03 08:26:19 +0300

    upd

--- tmux/detect-theme.sh
+++ tmux/detect-theme.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 THEME_DIR="$HOME/.config/tmux"
+tmux source-file "$THEME_DIR/theme-dark.conf"
 
-if [ "$(uname)" = "Darwin" ]; then
-    if [ "$(defaults read -g AppleInterfaceStyle 2>/dev/null)" = "Dark" ]; then
-        tmux source-file "$THEME_DIR/theme-dark.conf"
-    else
-        tmux source-file "$THEME_DIR/theme-light.conf"
-    fi
-else
-    tmux source-file "$THEME_DIR/theme-dark.conf"
-fi
+# if [ "$(uname)" = "Darwin" ]; then
+#     if [ "$(defaults read -g AppleInterfaceStyle 2>/dev/null)" = "Dark" ]; then
+#         tmux source-file "$THEME_DIR/theme-dark.conf"
+#     else
+#         tmux source-file "$THEME_DIR/theme-light.conf"
+#     fi
+# else
+#     tmux source-file "$THEME_DIR/theme-dark.conf"
+# fi
