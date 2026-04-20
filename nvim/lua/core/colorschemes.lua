954e3d832f7c821253f9b112570ee272a1178862
bells307 <bells.dev@proton.me>
2026-04-20 11:22:24 +0300

    upd

--- nvim/lua/core/colorschemes.lua
+++ nvim/lua/core/colorschemes.lua
@@ -17,14 +17,18 @@ local function apply_colorscheme()
 		-- '~' lines after EOF
 		vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#938AA9" })
 
+		local function hl_fg(name)
+			local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+			return hl.fg and string.format("#%06x", hl.fg) or nil
+		end
 		-- telescope floating window
-		vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE", fg = hl_fg("Normal") })
 		-- telescope border
-		vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE", fg = hl_fg("Special") })
 		-- input prompt area
-		vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "NONE", fg = hl_fg("Normal") })
 		-- results list area
-		vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "NONE", fg = hl_fg("Normal") })
 		-- file preview area
 		vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "NONE" })
 	end
